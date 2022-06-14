6dff7060b1d01287642c568b045a46e73962b66d
Gabriel <gabrielmaes@hotmail.com>
2022-06-14 18:35:15 +0200

    added my project

---
+++ script.js
@@ -0,0 +1,50 @@
+function verify() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert(' [ERROR] Check the input and try again.')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'man'
+            if (idade >= 0 && idade <= 10) {
+                //kid
+                img.setAttribute('src', 'foto-bebe-m.png')
+             } else if (idade < 21) {
+                //young
+                img.setAttribute('src', 'foto-jovem-m.png')
+              } else if (idade <50) {
+                //adult
+                img.setAttribute('src', 'foto-adulto-m.png')
+               } else {
+                //elderly
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'woman'
+            if (idade >= 0 && idade <= 10) {
+                //kid
+                img.setAttribute('src', 'foto-bebe-f.png')
+             } else if (idade < 21) {
+                //young
+                img.setAttribute('src', 'foto-jovem-f.png')
+              } else if (idade <50) {
+                //adult
+                img.setAttribute('src', 'foto-adulto-f.png')
+               } else {
+                //elderly
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+        }
+        res.innerHTML = `You are a ${gênero} with ${idade} years old`
+        res.style.textAlign = 'center'
+        res.appendChild(img)
+    }
+
+}
